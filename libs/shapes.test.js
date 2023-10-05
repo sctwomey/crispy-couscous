e0d67fc0075c1f1ba9fd36083680c1e7884d458d
sctwomey <sctwomey@gmail.com>
2023-10-05 15:21:57 -0700

    Refactor test js file

--- libs/shapes.test.js
+++ libs/shapes.test.js
@@ -5,8 +5,10 @@ const { Square, Circle, Triangle } = require('./shapes.js');
 describe('Triangle', () => {
     describe('color', () => {
         it("should have a green fill color displayed in a triangle shape", () => {
+
             const newTriangle = `<polygon points="150 18, 244 182, 56 182" fill="green" />`;
             const shape = new Triangle();
+
             shape.setColor('green');
             expect(shape.render()).toEqual(newTriangle);
         });
@@ -17,8 +19,10 @@ describe('Triangle', () => {
 describe('Square', () => {
     describe('color', () => {
         it("should have a green fill color displayed in a square shape", () => {
+
             const newSquare = `<rect x="75" y="25" width="150" height="150" fill="green" />`;
             const shape = new Square();
+
             shape.setColor('green');
             expect(shape.render()).toEqual(newSquare);
         });
@@ -28,9 +32,11 @@ describe('Square', () => {
 // Testing a circle shape for a green fill color.
 describe('Circle', () => {
     describe('color', () => {
+
         it("should have a green fill color displayed in a circle shape", () => {
             const newCircle = `<circle cx="150" cy="100" r="80" fill="green" />`;
             const shape = new Circle();
+
             shape.setColor('green');
             expect(shape.render()).toEqual(newCircle);
         });
